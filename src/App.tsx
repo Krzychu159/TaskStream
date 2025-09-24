fdbc10839f71f5eb0eb27ebe1d333e68c5f1043d
Krzychu159 <krzysztof.rak159@gmail.com>
2025-09-24 21:04:58 +0200

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 export default function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-red-600 underline">
-        Hello Tailwind + Vite!
-      </h1>
+      <h1 className="text-4xl font-bold text-red-600 underline"></h1>
     </div>
   );
 }
